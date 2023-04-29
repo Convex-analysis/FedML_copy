52d9d3d8808f6d60dd2346febe4e42c6f4e1eec3
Convex-analysis <799447128@qq.com>
2023-04-29 23:09:27 +0800

    implement the client_sampling function

--- python/fedml/simulation/sp/multi_hierarchical_FL/cloud.py
+++ python/fedml/simulation/sp/multi_hierarchical_FL/cloud.py
@@ -1,5 +1,5 @@
 import logging
-
+import numpy as np
 from .group import Group
 from ..hierarchical_fl.trainer import HierarchicalTrainer
 
@@ -24,12 +24,33 @@ class Cloud(HierarchicalTrainer):
                 self.args.client_num_in_total,
                 self.args.client_num_per_round,
             )
+            
+
 
     def _client_sampling(self, global_round_idx, group_list, client_num_per_round):
         #store all groups' client and ready to sample
         total_canadiate_client = []
         for group in group_list:
             total_canadiate_client.extend(group.client_dict.keys())
+        if total_canadiate_client == client_num_per_round:
+            client_indexes = [client_index for client_index in range(total_canadiate_client)]
+        else:
+            num_clients = min(client_num_per_round, total_canadiate_client)
+            np.random.seed(global_round_idx)  # make sure for each comparison, we are selecting the same clients each round
+            client_indexes = np.random.choice(range(total_canadiate_client), num_clients, replace=False)
+        logging.info("client_indexes = %s" % str(client_indexes))
+        
+        group_to_client_indexes = {}
+        for client_idx in client_indexes:
+            group_idx = self.group_indexes[client_idx]
+            if not group_idx in group_to_client_indexes:
+                group_to_client_indexes[group_idx] = []
+            group_to_client_indexes[group_idx].append(client_idx)
+        logging.info(
+            "client_indexes of each group = {}".format(group_to_client_indexes)
+        )
+        return client_indexes
+    
 
             
 
